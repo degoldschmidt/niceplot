88897a7b7e3ba7e07d196e1d1bdf6bc56a6f83d4
degoldschmidt <goldschmidt.de@gmail.com>
2017-09-16 15:08:14 +0100

    Style set_font working

--- niceplot/styla.py
+++ niceplot/styla.py
@@ -15,32 +15,19 @@ def set_font(name, ax=None):
     if os.path.exists(fontfile):
         print("Loading font:", fontfile)
         textprop = fm.FontProperties(fname=fontfile)
-
+        ### find all text objects
         texts = ax.findobj(match=mpl_text.Text)
-        print(texts)
-
-        ### titles/labels
-        ax.set_title(ax.get_title(), fontproperties=textprop)
-        ax.set_xlabel(ax.get_xlabel(), fontproperties=textprop)
-        ax.set_ylabel(ax.get_ylabel(), fontproperties=textprop)
-        ### ticks
-        for label in ax.get_xticklabels():
-            label.set_fontproperties(textprop)
-        for label in ax.get_yticklabels():
-            label.set_fontproperties(textprop)
-        ### legend
-        if not ax.get_legend() is None:
-            for text in ax.get_legend().get_texts():
-                text.set_fontproperties(textprop)
-        ### table
-        tables = ax.findobj(match=table.Table)
+        for eachtext in texts:
+            eachtext.set_fontproperties(textprop)
+        ### find all table objects (these contain further text objects)
+        tables = ax.findobj(match=mpl_table.Table)
         for eachtable in tables:
-            for k, cell in eachtable._cells.items():
-                this_text = cell._text.get_text()
+            for k, eachcell in eachtable._cells.items():
+                this_text = eachcell._text.get_text()
                 if this_text == u"\u25CF" or this_text == u"\u25CB":
-                    cell._text.set_fontname("Arial Unicode MS")
+                    eachcell._text.set_fontname("Arial Unicode MS")
                 else:
-                    cell._text.set_fontproperties(textprop)
+                    eachcell._text.set_fontproperties(textprop)
     else:
         print(fontfile, os.getcwd())
         print("[ERROR]: selected font does not exist.")
